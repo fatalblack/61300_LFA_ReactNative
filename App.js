6c4763bde33b5b39c1ed6ab102d0e21a8133f79e
Luis Fernando Aguirre <luis.aguirre@plataforma10.com>
2024-01-18 10:55:16 -0300

    Clase 2 - primer commit

--- App.js
+++ App.js
@@ -3,10 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={stylesHeader.container}>
+        <Text>Aquí irá el header de la tienda</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>Hola, Coder!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={stylesFooter.container}>
+        <Text>Aquí irá el footer</Text>
+      </View>
+    </>
   );
 }
 
@@ -16,5 +24,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const stylesHeader = StyleSheet.create({
+  container: {
+    backgroundColor: '#cceeff',
+    alignItems: 'left',
+    justifyContent: 'center',
+    height: 100,
+    padding: 10
+  },
+});
+
+const stylesFooter = StyleSheet.create({
+  container: {
+    backgroundColor: '#cceeff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    padding: 10
   },
 });
